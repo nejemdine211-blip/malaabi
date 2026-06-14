d7de7d35825baa080a018f7566326ab17fd4039c
nejemdine <nejemdime211@gmail.com>
2026-06-14 04:32:10 +0100

    fix OneSignal prompt

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,17 @@ import OneSignal from 'react-onesignal'
 OneSignal.init({
   appId: '49f1f9f9-b16a-46e7-b8b8-3c6a28d166d6',
   allowLocalhostAsSecureOrigin: true,
+  notifyButton: {
+    enable: true,
+  },
+  promptOptions: {
+    slidedown: {
+      enabled: true,
+      autoPrompt: true,
+      timeDelay: 3,
+      pageViews: 1,
+    }
+  }
 })
 
 createRoot(document.getElementById('root')).render(
