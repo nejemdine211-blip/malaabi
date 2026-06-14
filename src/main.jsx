3d057be5fb6000f9f0d92ef18cdbee4add152113
nejemdine <nejemdime211@gmail.com>
2026-06-14 04:21:51 +0100

    add OneSignal push notifications

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import OneSignal from 'react-onesignal'
+
+OneSignal.init({
+  appId: '49f1f9f9-b16a-46e7-b8b8-3c6a28d166d6',
+  allowLocalhostAsSecureOrigin: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
