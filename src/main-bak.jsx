0de4aa9b30487a1600a2fbf20757fdf1b2f6d49c
nejemdine <nejemdime211@gmail.com>
2026-09-03 20:22:59 +0000

    feat: add Sentry error monitoring

--- src/main-bak.jsx
+++ src/main-bak.jsx
@@ -3,13 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import OneSignal from 'react-onesignal'
-import * as Sentry from '@sentry/react'
-
-Sentry.init({
-  dsn: 'https://886f8c9aa1c0f0605063c63c489de415@o4512024240390144.ingest.de.sentry.io/4512024265424976',
-  environment: import.meta.env.MODE,
-  tracesSampleRate: 0,
-})
 
 OneSignal.init({
   appId: '49f1f9f9-b16a-46e7-b8b8-3c6a28d166d6',
